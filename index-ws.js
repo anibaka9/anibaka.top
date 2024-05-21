4e55d3e3dcb467ef54f64fc83f41e54b3b483fda
Aleskei Budkin <anibaka9@gmail.com>
2024-05-22 00:17:22 +0600

    added db logging

--- index-ws.js
+++ index-ws.js
@@ -1,21 +1,30 @@
 const express = require("express");
-const server = require("node:http").createServer();
+const server = require("http").createServer();
 const app = express();
 
-app.get("/", (req, res) => {
+app.get("/", function (req, res) {
   res.sendFile("index.html", { root: __dirname });
 });
 
 server.on("request", app);
-server.listen(3000, () => {
+server.listen(3000, function () {
   console.log("server started on port 3000");
 });
 
 /** Begin websocket */
-
 const WebSocketServer = require("ws").Server;
 
 const wss = new WebSocketServer({ server: server });
+
+process.on("SIGINT", () => {
+  console.log("sigint");
+  wss.clients.forEach(function each(client) {
+    client.close();
+  });
+  server.close(() => {
+    shutdownDB();
+  });
+});
 
 wss.on("connection", function connection(ws) {
   const numClients = wss.clients.size;
@@ -27,14 +36,45 @@ wss.on("connection", function connection(ws) {
     ws.send("Welcome to my server");
   }
 
+  db.run(`INSERT INTO visitors (count, time)
+        VALUES (${numClients}, datetime('now'))
+    `);
+
   ws.on("close", function close() {
     wss.broadcast(`Current visitors: ${numClients}`);
-    console.log("A client has disconected");
+    console.log("A client has disconnected");
   });
 });
 
-wss.broadcast = (data) => {
-  for (client of wss.clients) {
+wss.broadcast = function broadcast(data) {
+  wss.clients.forEach(function each(client) {
     client.send(data);
-  }
+  });
 };
+
+/** end websockets */
+/** begin database */
+const sqlite = require("sqlite3");
+const db = new sqlite.Database(":memory:");
+
+db.serialize(() => {
+  db.run(`
+        CREATE TABLE visitors (
+            count INTEGER,
+            time TEXT
+        )
+    `);
+});
+
+function getCounts() {
+  db.each("SELECT * FROM visitors", (err, row) => {
+    console.log(row);
+  });
+}
+
+function shutdownDB() {
+  console.log("Shutting down db");
+
+  getCounts();
+  db.close();
+}
